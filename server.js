037a70e16fc1204c07cc02370f5fd6fec039beb3
sergio <sergio.miranda@medismart.live>
2023-01-03 03:44:20 -0300

    first

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+   
+        res.sendFile(path.resolve("./public/index.html"));
+     
+});
+
+app.listen(3000, console.log('el servidor esta en el puerto 3000'))
